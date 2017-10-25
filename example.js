f8965b713ff657010d7b2eabd9fe006c27021b42
Shakhor Smith <shakhorsmith@gmail.com>
2017-10-25 00:34:31 -0500

    Finished example for class

---
+++ example.js
@@ -0,0 +1,22 @@
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+*/
+
+function checkPalindrome(inputString) {
+    let reverse = inputString.split('').reverse().join('');
+
+    if (inputString === reverse) {
+        return true
+    }
+
+    return false
+}
