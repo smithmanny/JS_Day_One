5257c546b330b71e54a46c5378224524810779fc
Shakhor Smith <shakhorsmith@gmail.com>
2017-10-25 01:03:11 -0500

    clean up code

--- example.js
+++ example.js
@@ -14,8 +14,6 @@ function truncateString(str, num) {
   if (str.length > num && num > 3) {
     return str.slice(0, num - 3) + '...';
   }
-
-  return str;
 }
 
 console.log(truncateString('A-tisket a-tasket A green and yellow basket', 11));
