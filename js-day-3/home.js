815fc099ed94eb51ea32992d69836d77caad0f67
Shakhor Smith <shakhorsmith@gmail.com>
2017-10-26 22:56:28 -0500

    Added map and reduce to answer

--- js-day-3/home.js
+++ js-day-3/home.js
@@ -3,20 +3,23 @@ Return an array consisting of the largest number from each provided sub-array. F
 */
 
 const largestOfFour = (arr) => {
-  let results = [];
-  for (let n = 0; n < arr.length; n++) {
-    let largestNumber = arr[n][0];
+  // let results = [];
+  // for (let n = 0; n < arr.length; n++) {
+  //   let largestNumber = arr[n][0];
+  //
+  //   for (let sb = 1; sb < arr[n].length; sb++) {
+  //     if (arr[n][sb] > largestNumber) {
+  //       largestNumber = arr[n][sb];
+  //     }
+  //   }
+  //
+  //   results[n] = largestNumber;
+  // }
+  //
+  // return results;
 
-    for (let sb = 1; sb < arr[n].length; sb++) {
-      if (arr[n][sb] > largestNumber) {
-        largestNumber = arr[n][sb];
-      }
-    }
-
-    results[n] = largestNumber;
-  }
-
-  return results;
+  return arr.map((val) => val.reduce((prev, current) =>
+  current > prev ? current : prev))
 }
 
 const test = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
